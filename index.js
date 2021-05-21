86bd14f69c68c23b063f633dbe0a46e2735ed4a5
Krae <kraewind@gmail.com>
2021-05-21 11:35:20 -0400

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,45 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      if (typeof collection === "array") {
+        collection.forEach(element => callback(element, collection.indexOf(element), collection))
+      } else {
+        for (const key in collection) {
+          callback(collection[key], key, collection)
+        }
+      }
+      return collection
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let col = []
+      if (typeof collection === "array") {  
+        collection.forEach(element => col.push(callback(element, collection.indexOf(element), collection)))
+      } else {
+        for (const key in collection) {
+          col.push(callback(collection[key], key, collection))
+        }
+      }
+      return col
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      console.log(acc)
+      console.log(collection)
+      console.log(callback(2))
+      const initial = acc
+      let i
+      if (acc != null) {
+        for (i = 0; i < collection.length; i++) {
+          acc = callback(initial, collection[i], collection)
+        }
+      } else {
+        for (i = 0; i < collection.length; i++) {
+          acc = callback(0, collection[i], collection)
+        }
+      }
+      return acc
     },
 
     functions: function() {
